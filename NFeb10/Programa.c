076d9aeee4521167e7a4865749584056efd47300
LeonardoDanielZavalaCuellar <ld.zavalacuellar@ugto.mx>
2025-02-10 11:51:28 -0600

    Act de clase

---
+++ NFeb10/Programa.c
@@ -0,0 +1,11 @@
+/* Este es una Segunda prueba en c */
+
+#include<stdio.h>
+
+int main(){
+
+printf("WENAS TARDES /n");
+printf(" Gracias por ayudarme a solucionar el error 403 /n");
+}
+
+exit (0);
