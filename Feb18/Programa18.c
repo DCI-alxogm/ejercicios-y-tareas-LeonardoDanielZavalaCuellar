befc9a38d2266836abc40a01c3b474287314d7f4
LeonardoDanielZavalaCuellar <ld.zavalacuellar@ugto.mx>
2025-02-17 11:40:25 -0600

    act febrero

---
+++ Feb18/Programa18.c
@@ -0,0 +1,47 @@
+//Ejemplo operaciones Leonardo Daniel Zavala Cuellar
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main () {
+
+int a,b,c,d;
+int e1,e2,e3,e4;
+a=3;
+b=4;
+c=5;
+d=7;
+e1=(a+b)*c/d;
+e2=((a+b)*c)/d;
+e3=(a+b)*(c/d);
+e4=a+(b*c)/d;
+
+
+printf("e1=(a+b)*c/d=%d \n",e1);
+printf("e2=((a+b)*c)/d=%d \n",e2);
+printf("e3=(a+b)*(c/d)=%d \n",e3);
+printf("e4=a+(b*c)/d=%d \n",e4);
+
+exit (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
