68a107196b3e85825b0782e4cb42b0c5b9c28016
LeonardoDanielZavalaCuellar <ld.zavalacuellar@ugto.mx>
2025-02-19 11:52:12 -0600

    Ejercicio clase 19 Febrero

---
+++ Feb19/TransCoo.c
@@ -0,0 +1,59 @@
+/*Programacion basica Ejercicio Leonardo Daniel Zavala Cuellar 
+19/02/2025*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main (){
+float x,y,z;
+
+float r,tta,agul,r1,tta1,z1;
+
+printf("Introduzca el valor a:\n");
+scanf("%f",&x);
+printf("Introduzca el valor b:\n");
+scanf("%f",&y);
+printf("Introduzca el valor c:\n");
+scanf("%f",&z);
+//esfericas
+r= (x*x)+(y*y)+(z*z);
+tta= arctan(y/x);
+agul= arcos(z/(sqrt((x*x)+(y*y)+(z*z)));
+//cilindricas
+r=(x*x)+(y*y);
+tta1= arctan(y/x);
+z1= z
+
+exit (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
