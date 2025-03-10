0e173e7f4121e94c084afe832d2a906245802409
LeonardoDanielZavalaCuellar <ld.zavalacuellar@ugto.mx>
2025-03-10 11:46:00 -0600

    Activida en clase 10 de Marzo

--- Mar10/SwitchT.c
+++ Mar10/SwitchT.c
@@ -11,17 +11,17 @@ float Tc,Tk;
 printf("Wenas Tardes\n");
 denuevo:
 printf("Introduzca la temperatura que desea calcular de la siguiente manera\n");
-printf("Para pasar de Celsius a Kelvin escriba Kelvin\n");
-printf("Para pasar de Kelvin a Celsius escriba Celsius\n");
-scanf("%s",Temp)
+printf("Para pasar de Celsius a Kelvin escriba K\n");
+printf("Para pasar de Kelvin a Celsius escriba C\n");
+scanf("%s",&Temp);
 switch(Temp){
-case 'Kelvin':
+case 'K':
 printf("Wenas introduzca la temperatura que desea pasar a Kelvin Recuerde ponerla en Celsius Gracias\n");
 scanf("%f",&Tc);
 Tk=Tc + 273.15;
 printf("Sus temperatura es %fC° a Kelvin son %fK\n",Tc,Tk);
 break;
-case 'Celsius':
+case 'C':
 printf("Wenas introduzca la temperatura que desea pasar a Celsius Recuerde ponerla en Kelvin Gracias\n");
 scanf("%f",&Tk);
 Tc=Tk - 273.15;
