2ef21c6dc2a7529781c2eaff0e8b2362a027ff6c
LeonardoDanielZavalaCuellar <ld.zavalacuellar@ugto.mx>
2025-03-12 16:00:07 -0600

    Tarea completa

--- Tareas/Tarea2.c
+++ Tareas/Tarea2.c
@@ -5,25 +5,31 @@
 #include <math.h>
 int main ()
 {
+    //Todas las variables utilizadas
 float Tc,Kh,Lg,Kg,x,y,z;
 float tta,phi,r,r1,tta1,z1,ft,Tf,Mh,Lb;
 char op;
 char opc1[2];
 char opc[11];
 Regreso:
+//SOLIcita al usuario que diga que quiere calcular
 printf("WENAS TARDES\n");
 printf("Que desea Calcular Transformacion a unidades del sistema ingles o Coordenadas \n");
 printf("Para sistema ingles escriba S\n");
 printf("Para sistema Coordenadas C\n");
 scanf("%s",&op);
 switch(op){
+    //PARA coordenadas
 case 'C':
 reg:
+//Se solicitan el tipo de coordenadas que desea calcular
 printf("Escriba el tipo de coordenadas que desea calcular Esfericas o Cilindricas\n");
 scanf("%11s",opc);
+//EN caso de cilindricas
 if (strcmp(opc,"Cilindricas")==0)    
     {
 Nuez:
+//Operaciones
         printf("Introduzca el valor x:\n");
 scanf("%f",&x);
 printf("Introduzca el valor y:\n");
@@ -33,16 +39,21 @@ scanf("%f",&z);
 r1=sqrt((x*x)+(y*y));
 tta1= atan(y/x);
 z1= z;
+//IMPrime resultado
 printf("Los resultados de sus coordenadas cilindricas es\nr=%f\nThetta=%f\nz=%f\n",r1,tta1,z1);
 denuevo1:
+//Pregunta si quiere intetar denuevo
 printf("¿Quieres realizar otra operacion?\n si/no\n");
 scanf("%2s",opc1);
+//En caso de si 
 if (strcmp(opc1,"si")==0)
 {
 goto Nuez;
+//en caso de no
 }else if (strcmp(opc1,"no")==0)
 {
     printf("Gracias nos vemos\n");
+    //Cualquier otra respuesta
 }else{
     printf("Solo acepto si o no intente denuevo\n");
     goto denuevo1;
@@ -77,16 +88,18 @@ goto Nuez1;
 // En caso de que no seleccione Esfericas o Cilindricas
 } else{
         printf("Solo acepto Cilindricas o Esfericas intente denuevo\n");
-    goto reg;
-    }
-case 'S'
+}
+break;
+//En caso de sistema ingles
+case 'S':
+er:
+//Solicita que quire pasar a ese sitema
 printf("Escriba la medida que desea obtener Longitud,Masa,Velocidad,Temperatura\n");
 scanf("%11s",opc);
-reg1:
 if (strcmp(opc,"Longitud")==0)    
-    {
+ {
 Va:
-printf("Introduzca su longitud (en METROS) a pasar a Sistema ingles\n");
+printf("Introduzca su longitud (en Metros) a pasar a Sistema ingles\n");
 scanf("%f",&Lg);
 ft=Lg * 3.281;
 printf("Su longitud es %f m y en Sistema ingles es %f ft\n",Lg,ft);
@@ -103,9 +116,10 @@ goto Va;
     printf("Solo acepto si o no intente denuevo\n");
     goto fallo;
 }
-reg2:
-if (strcmp(opc,"Masa")==0)    
-    {
+break;
+
+}else if (strcmp(opc,"Masa")==0)
+{
 Va1:
 printf("Introduzca su Masa (en Kilogramos) a pasar a Sistema ingles\n");
 scanf("%f",&Kg);
@@ -124,26 +138,59 @@ goto Va1;
     printf("Solo acepto si o no intente denuevo\n");
     goto fallo1;
 }
+} else if (strcmp(opc,"Temperatura")==0)    
+    {
+Va2:
+printf("Introduzca su Temperatura (en Celcius) a pasar a Sistema ingles\n");
+scanf("%f",&Tc);
+Tf=(Tc * 1.8) + 32;
+printf("Su Temperatura es %f C° y en Sistema ingles es %f F\n",Tc,Tf);
+fallo2:
+printf("¿Quieres realizar otra operacion?\n si/no\n");
+scanf("%2s",opc1);
+if (strcmp(opc1,"si")==0)
+{
+goto Va2;
+}else if (strcmp(opc1,"no")==0)
+{
+    printf("Gracias nos vemos\n");
+}else{
+    printf("Solo acepto si o no intente denuevo\n");
+    goto fallo2;
+}
+}else if (strcmp(opc,"Velocidad")==0)    
+    {
+Va3:
+printf("Introduzca su Velocidad (en Km/h) a pasar a Sistema ingles\n");
+scanf("%f",&Kh);
+Mh= Kh * 0.62137;
+printf("Su Velocidad es %f Km/h y en Sistema ingles es %f M/h\n",Kh,Mh);
+fallo3:
+printf("¿Quieres realizar otra operacion?\n si/no\n");
+scanf("%2s",opc1);
+if (strcmp(opc1,"si")==0)
+{
+goto Va3;
+}else if (strcmp(opc1,"no")==0)
+{
+    printf("Gracias nos vemos\n");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}else{
+    printf("Solo acepto si o no intente denuevo\n");
+    goto fallo3;
+}
+}else{ 
+    //en caso de no seleccionar ninguna de las opciones
+    printf("solo acepto Longitud,Masa,Temperaturam o Velocidad intente denuevo\n");
+goto er;
+}
+break;
+default:
+//Default en caso de que no se seleccione nada de lo que se puede calcular
+printf("Opcion no valida intente denuevo\n");
+goto Regreso;
+break;
+}
 
 exit (0);
 }
