22c45f025898d738418a30298810db454256e9f3
LeonardoDanielZavalaCuellar <ld.zavalacuellar@ugto.mx>
2025-04-07 11:44:26 -0600

    Diagrama y Beta Abril 7

---
+++ Abr07/TempArr.c
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Leonardo Daniel Zavala Cuellar Abril 7 de 2025 BETA
+ * Arreglo Temperatura
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    
+    float TCi,Tf,n,delta,TC;
+    printf("WENAS TARDES\n");
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    return 0;
+}
