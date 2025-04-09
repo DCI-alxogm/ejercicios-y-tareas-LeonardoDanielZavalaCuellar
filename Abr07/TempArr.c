10558ae0ee47ed922002c8f14a7924f5b8913386
LeonardoDanielZavalaCuellar <ld.zavalacuellar@ugto.mx>
2025-04-09 10:07:59 -0600

    Tarea

--- Abr07/TempArr.c
+++ Abr07/TempArr.c
@@ -4,21 +4,35 @@
 *******************************************************************************/
 #include <stdio.h>
 
-int main()
-{
-    
-    float TCi,Tf,n,delta,TC;
-    printf("WENAS TARDES\n");
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+int main() {
+    float tempInicial, tempFinal, aumento;
+    int repeticiones;
+
+    printf("Ingrese la temperatura inicial: ");
+    scanf("%f", &tempInicial);
+    printf("Ingrese la temperatura final: ");
+    scanf("%f", &tempFinal);
+    printf("Ingrese el numero de repeticiones: ");
+    scanf("%d", &repeticiones);
+    printf("Ingrese el aumento por repeticion: ");
+    scanf("%f", &aumento);
+
+    float temperaturas[repeticiones];
+    int contador = 0;
+
+    for(int i = 0; i < repeticiones; i++) {
+        float temp = tempInicial + i * aumento;
+        if(temp > tempFinal) {
+            break;
+        }
+        temperaturas[contador] = temp;
+        contador++;
+    }
+
+    for(int i = 0; i < contador; i++) {
+        float kelvin = temperaturas[i] + 273.15;
+        printf("Temperatura %d: %.2f °C = %.2f K\n", i + 1, temperaturas[i], kelvin);
+    }
 
     return 0;
 }
